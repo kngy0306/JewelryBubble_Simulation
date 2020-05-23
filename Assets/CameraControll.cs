78a5181140f0211dee19b96283657693697a3a8f
Kona <kitami.kitano20@gmail.com>
2020-05-23 20:50:29 +0900

    first commit

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CameraControll : MonoBehaviour
+{
+    private GameObject mainCamera;              //メインカメラ格納用
+    private GameObject cubeObject;            //回転の中心となるプレイヤー格納用
+    public float rotateSpeed = 0.1f;            //回転の速さ
+ 
+    void Start()
+    {
+        //メインカメラと被写体をそれぞれ取得
+        mainCamera = Camera.main.gameObject;
+        cubeObject = GameObject.Find("Cube");
+
+        // 角度取得↓
+        Debug.Log(mainCamera.transform.localEulerAngles.x);
+    }
+ 
+    void Update()
+    {
+        rotateCamera();
+    }
+ 
+    private void rotateCamera()
+    {
+        //Vector3でX,Y方向の回転の度合いを定義
+        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, Input.GetAxis("Mouse Y") * rotateSpeed, 0);
+ 
+        // transform.RotateAround()をしようしてメインカメラを回転させる
+        if(Input.GetKey("space")){  // 観測方向
+            // RotateAround(中心のobj, 軸, 角度)
+            mainCamera.transform.RotateAround(cubeObject.transform.position, Vector3.up, angle.x);
+        }else{ // 観測角度
+        //if(mainCamera.transform.rotation.z > -0.8f){    カメラの動き制限やろうとした
+            mainCamera.transform.RotateAround(cubeObject.transform.position, Vector3.right, angle.y);
+        //}    
+        }
+    }
+}
---
+++ Assets/ChangeColorRGBA.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorRGBA : MonoBehaviour {
+
+    private GameObject mainCamera;
+    public int angle, color_red, color_green, color_blue;
+
+    private string[] strR;
+    private string[] strG;
+    private string[] strB;
+
+    void Start (){
+        mainCamera = Camera.main.gameObject;
+
+        string fileR = "C:/Users/kona/image_red.txt";
+        string fileG = "C:/Users/kona/image_green.txt";
+        string fileB = "C:/Users/kona/image_blue.txt";
+
+        StreamReader srR = new StreamReader(fileR);
+        StreamReader srG = new StreamReader(fileG);
+        StreamReader srB = new StreamReader(fileB);
+
+        while (srR.Peek() >= 0)
+        {
+            strR = srR.ReadLine().Split(' ');
+            strG = srG.ReadLine().Split(' ');
+            strB = srB.ReadLine().Split(' ');
+        }
+        Debug.Log(strR[0]);
+        Debug.Log(strG[0]);
+
+        srR.Close();
+        srG.Close();
+        srB.Close();
+    }
+
+    void Update () {
+        angle = (int)mainCamera.transform.localEulerAngles.x;
+
+        if (0 > angle || angle > 90) {
+            Color color = new Color32(255, 255, 255, 0);
+            GetComponent<Renderer>().material.color = color;
+        } else {
+            color_red = int.Parse(strR[angle]);
+            color_green = int.Parse(strG[angle]);
+            color_blue = int.Parse(strB[angle]);
+
+            Color color = new Color32((byte)color_red, (byte)color_green, (byte)color_blue, 1);
+            GetComponent<Renderer>().material.color = color;
+        }
+        
+    }
+}
