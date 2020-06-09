5fdb0b220ee343ef4716f3f9b23e0733de126dcf
Kona <kitami.kitano20@gmail.com>
2020-06-09 14:45:38 +0900

    Cylinder作成

---
+++ Assets/CC.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CC : MonoBehaviour {
+
+    private GameObject mainCamera;
+    private GameObject polarizer;
+
+    private int angle, rotate, color_red, color_green, color_blue;
+    private string[, , ] rgb = new string[90, 360, 3];
+
+    void Start () {
+        mainCamera = Camera.main.gameObject;
+        polarizer = GameObject.FindGameObjectWithTag ("Polarizer");
+
+        //fileの読み込み -> 0.5mの画像
+        string fileName = "C:/Users/kona/image_end.txt";
+        var arrText = new List<string> ();
+        StreamReader objReader = new StreamReader (fileName);
+
+        string sLine = ""; // ←一時格納用
+
+        // ファイルから1行ずつ読み込む
+        while (sLine != null) {
+            sLine = objReader.ReadLine ();
+            if (sLine != null)
+                arrText.Add (sLine); // addメソッドで追記
+        }
+        objReader.Close ();
+
+        // --- ArrayListから2次元配列に格納する．---
+        int cnt = 0;
+        string[, ] array = new string[1, 97200]; // 最初に一列で保存するための配列
+        foreach (string sOutput in arrText) {
+            string[] temp_line = sOutput.Split (' ');
+            foreach (string value in temp_line) {
+                if (cnt < 97200) {
+                    array[0, cnt] = value;
+                    cnt++;
+                }
+            }
+        }
+
+        int column, row, color, temporary;
+        for (int i = 0; i < 97200; i++) {
+            temporary = i % 270;
+            column = temporary / 3;
+            row = i / 270;
+            color = i % 3;
+
+            rgb[column, row, color] = array[0, i];
+        }
+
+        Debug.Log (rgb[53, 0, 0]);
+    }
+
+    void Update () {
+        angle = (int) mainCamera.transform.localEulerAngles.x;
+        rotate = (int) polarizer.transform.localEulerAngles.y;
+
+        if (0 > angle || angle > 90) {
+            Color color = new Color32 (255, 255, 255, 0);
+            GetComponent<Renderer> ().material.color = color;
+        } else {
+            //      rgb[ 観測角度(0~90°), 偏光版角度(0~360°), 色rgb(0~2) ]
+            color_red = int.Parse (rgb[angle, rotate, 0]);
+            color_green = int.Parse (rgb[angle, rotate, 1]);
+            color_blue = int.Parse (rgb[angle, rotate, 2]);
+
+            Color color = new Color32 ((byte) color_red, (byte) color_green, (byte) color_blue, 1);
+            GetComponent<Renderer> ().material.color = color;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
+### 6/9
+
+Cylinder3 つ作成  
+それぞれに氷の厚さが異なる image を読み込ませる
+
 # JewelryBubble_Simulation
+
 Unity- JewelryBubble_Simulation
 
 ### Screen of Unity
+
 ![scshot](https://user-images.githubusercontent.com/57553474/82730117-446dd700-9d38-11ea-8426-04627770978d.png)
 
-### 角度0°~90° 偏光版角度0°
-![0 5mm__0-90](https://user-images.githubusercontent.com/57553474/82730119-4cc61200-9d38-11ea-9f69-17312cde7bda.png)
+### 角度 0°~90° 偏光版角度 0°
 
+![0 5mm__0-90](https://user-images.githubusercontent.com/57553474/82730119-4cc61200-9d38-11ea-9f69-17312cde7bda.png)
