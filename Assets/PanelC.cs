e43fb781800a7a137cc36b29f22a01bce1ea90e1
Kona <kitami.kitano20@gmail.com>
2020-06-20 17:54:34 +0900

    create polarizer

---
+++ Assets/PanelC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelC : MonoBehaviour {
+    void Update () {
+        polarizerRotate ();
+    }
+
+    // 偏光版回転
+    private void polarizerRotate () {
+        if (Input.GetKey (KeyCode.RightArrow)) {
+            transform.Rotate (0, 0, -0.5f);
+        } else if (Input.GetKey (KeyCode.LeftArrow)) {
+            transform.Rotate (0, 0, 0.5f);
+        }
+    }
+}
